e414033d95b90f1e8827e6c8cd2a521dedd1ef8d
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-09 11:20:50 +0100

    Cats and dogs table and cats

--- Dyr Ice/src/sql/create_table.sql
+++ Dyr Ice/src/sql/create_table.sql
@@ -2,8 +2,21 @@ DROP TABLE IF EXISTS Dog;
 
 CREATE TABLE Dog
 (
-    name varchar(20) not null,
+    id integer primary key,
+    species varchar(50) not null,
+    cost INTEGER default 0,
     lifespan INTEGER default 0,
-    allergies Boolean default false,
-    price INTEGER default 0
+    temper text (100),
+    allergyFriendly boolean not null,
+
+);
+
+DROP TABLE IF EXISTS Cat;
+create table Cat(
+   id integer primary key,
+   species varchar(50) not null ,
+   origin varchar(50) not null,
+   lifeSpan integer not null,
+   temper text not null,
+   allergyFriendly boolean not null
 );
---
+++ Dyr Ice/src/sql/insert_Cat.sql
@@ -0,0 +1,156 @@
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (1,'Ragdoll','United States',17,'Gentle, Quiet, Friendly, Easy Going','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (2,'Exotic Shorthair','United States',15,'Quiet, Lovely, Warm pure, Sweet','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (3,'British Shorthair','United Kingdom',17,'Family, Loyal, Patient, Easy Going','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (4,'Persian','Iran(Persia)',15,'Gentle, Sensitive, Quiet, Ability','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (5,'Maine Coon','United States',15,'Gentle, Intelligent, Independent','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (6,'American Shorthair','United States',17,'Curious, Playful, Active, Easy Going','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (7,'Devon Rex','United Kingdom',15,'Loyal, Playful, Social','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (8,'Sphynx','Canada',14,'Gentle, Friendly, Quiet','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (9,'Scottish Fold','United Kingdom',14,'Intelligent, Playful, Affectionate, Loyal','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (10,'Abyssinian','Egypt',15,'Energetic, Independent, Intelligent, Gentle','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (11,'Oriental','United States',14,'Curious, Intelligent, Social, Playfulness','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (12,'Siamese','Thailand',15,'Lively, Active, Clever','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (13,'Norwegian Forest Cat','Norway',16,'Loving, Playful, Curious, Intelligent','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (14,'Cornish Rex','Cornwall, England',14,'Playful, Intelligent, Affectionate, Curious','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (15,'Bengal','United States',16,'Agile, Family, Inquisitive, Lively','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (16,'Russian Blue','Russia',16,'Easy Going, Intelligent, Loyal, Playful','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (17,'Siberian','Russia',15,'Loyal, Playful, Agile, Intelligent','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (18,'Burmese','Burma, Thailand',16,'Interactive, Curious, Family, Intelligent','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (19,'Birman','France',15,'Active, Social, Gentle','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (20,'Tonkinese','Canada',16,'Intelligent, Lively, Curious, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (21,'Ocicat','United States',14,'Active, Agile, Demanding, Family','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (22,'Selkirk Rex','United States',15,'Dependent, loving, Patient, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (23,'RagaMuffin','United States',16,'Friendly, Gentle, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (24,'American Curl','United States',16,'Family, Interactive, Lively, Loving','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (25,'Japanese Bobtail','Japan',16,'Family, Lively, Loyal, Outgoing','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (26,'Manx','Isle of man',14,'Easy Going, Intelligent, Loyal, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (27,'Egyptian Mau','Egypt',20,'Agile, Intelligent, Loyal, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (28,'Somali','Somalia',16,'Curious, Interactive, Loyal, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (29,'Balinese','United States',15,'Family, Intelligent, Loving, Playful','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (30,'Singapura','Singapore',15,'Curious, Easy Going, Family, Inquisitive','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (31,'Colorpoint Shorthair','United States',16,'Intelligent, Playful, Affectionate, Social','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (32,'Lykoi','United States',16,'Active, Playful, Intelligent, Loving','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (33,'Chartreux','France',15,'Loving, Loyal, Intelligent, Family','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (34,'Turkish Angora','Turkey',18,'Intelligent, Playful, Gentle, Clever','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (35,'European Burmese','Burma, Thailand',15,'Loyal, Sweet, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (36,'Bombay','United States',16,'Intelligent, Loving, Playful, Sweet','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (37,'Khao Manee','Thailand',12,'Active, Outgoing, Intelligent, Talkative','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (38,'Burmilla','United Kingdom',15,'Playful, Relaxed, Social, Sweet','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (39,'Korat','Thailand',15,'Quiet, Gentle, Intelligent, Friendly','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (40,'American Bobtail','United States',15,'Intelligent, Interactive, Lively, Family','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (41,'Havana Brown','United Kingdom',15,'Curious, Demanding, Friendly, Intelligent','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (42,'LaPerm','United States',14,'Clever, Intelligent, Playful, Friendly','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (43,'Turkish Van','Turkey',17,'Intelligent, Loyal, Playful, Talkative','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (44,'American Wirehair','United States',18,'Family, Lively, Outgoing, Interactive','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (45,'California Spangled','United States',14,'Intelligent, Curious, Loyal, Social','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (46,'Cymric','Canada',14,'Intelligent, Playful, Loyal, Gentle','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (47,'Himalayan','United States',15,'Dependent, Gentle, Intelligent, Quiet','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (48,'Munchkin','United States',15,'Agile, Easy Going, Intelligent, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (49,'Nebelung','United States',15,'Shy, Gentle, Quiet','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (50,'Pixie Bob','United States',16,'Sociable, Loyal, Intelligent, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (51,'Savannah','United States',20,'Curious, Loyal, Intelligent, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (52,'Snowshoe','United States',19,'Intelligent, Social, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (53,'Toyger','United States',15,'Intelligent, Playful, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (54,'York Chocolate','United States',15,'Curious, Friendly, Intelligent, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (55,'Aegean','Greece',12,'Intelligent, Playful, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (56,'Australian Mist','Australia',16,'Intelligent, Friendly','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (57,'American Polydactyl','United States',16,'Intelligent, Affectionate, Playful, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (58,'Arabian Mau','Arabian Peninsula (UAE)',14,'Independent, Curious, Agile, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (59,'Asian Cat','United Kingdom',18,'Affectionate, Interactive, Playful, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (60,'Asian Semi-longhair','United Kingdom',15,'Active, Curious, Gentle','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (61,'Bambino','United States',14,'Affectionate, Friendly, Intelligent','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (62,'Brazilian Shorthair','Brazil',20,'Active, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (63,'British Longhair','United Kingdom',15,'Friendly, Sociable, Loyal, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (64,'Chantilly-Tiffany','United States',16,'Demanding, Loyal, Interactive, Affectionate','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (65,'Chausie','Egypt',14,'Active, Outgoing, Affectionate, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (66,'Cheetoh','United States',14,'Gentle, Social, Intelligent, Affectionate','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (67,'Cyprus','Cyprus',15,'Affectionate, Social','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (68,'Javanese','United States',12,'Gentle, Social, Intelligent, Affectionate','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (69,'Donskoy','Russia',15,'Friendly, Affectionate, Intelligent, Sociable','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (70,'Peterbald','Russia',15,'Athletic, Playful, Friendly, Affectionate','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (71,'Ukrainian Levkoy','Ukraine',15,'Friendly, Playful, Intelligent, Inquisitive','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (72,'Elf Cat','United States',14,'Active, Intelligent, Friendly, Sociable','true');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (73,'Dwelf','United States',15,'Affectionate, Friendly, Lively, Sociable','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (74,'Sokoke','Kenya',15,'Lively, Loving, Intelligent, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (75,'MinSkin','United States',14,'Sweet-tempered, Friendly, Inquisitive, Intelligent','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (76,'Serengeti Cat','United States',15,'Confident, Energetic, Friendly, sociable','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (77,'Napoleon Cat','United States',15,'Loving, Sweet, Easy Going, Playful','false');
+insert into Cat(id, species, origin, lifeSpan, temper, allergyFriendly)
+values (78,'Thai Cat','Thailand',16,'Talkative, Playful, Loving, Affectionate','false');
