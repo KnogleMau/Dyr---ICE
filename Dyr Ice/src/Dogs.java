6d499e4b6fa8568ed8e262dbaa5025c8fbcacd5d
Thranum <thranum2405@gmail.com>
2024-12-10 14:23:47 +0100

    dogclass constructer

--- Dyr Ice/src/Dogs.java
+++ Dyr Ice/src/Dogs.java
@@ -1,2 +1,15 @@
-public class Dogs {
+public class Dogs extends Animal {
+public Dogs(int id, String species, int cost, int lifeSpan, String temper, String allergyFriendly) {
+    super(id, species, cost, lifeSpan, temper, allergyFriendly);
+}
+
+public String toString(){
+    return "id: " + this.id + ", " +
+            " species: " + this.species + ", " +
+            " cost: " + this.cost + ", " +
+            " lifespan: " + this.lifeSpan + ", " +
+            " temper: " + this.temper + ", " +
+            " allergyFriendly: " + this.AllergyFriendly +"!";
+
+    }
 }
