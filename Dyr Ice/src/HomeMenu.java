b93dc8715d8aa363a4326f1fa1c573f6568d7d8e
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-10 12:41:02 +0100

    implement switch example

--- Dyr Ice/src/HomeMenu.java
+++ Dyr Ice/src/HomeMenu.java
@@ -1,2 +1,25 @@
 public class HomeMenu {
+    private TextUI ui = new TextUI();
+    
+    public void displayMenu(){
+        int choice = ui.promptNumeric("Type a number:");
+
+        switch(choice){
+            case 1:
+                // search methods
+                break;
+            case 2:
+                // jobs.method
+                break;
+            case 3:
+                // profile
+                break;
+            case 4:
+                //logout
+                break;
+            default:
+                System.out.println("invalid, type a number");
+                displayMenu();
+        }
+    }
 }
