423cc29f1c7968b48abce2f777afe1b43c7c9be8
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-10 13:17:27 +0100

    cat constructor and toString

--- Dyr Ice/src/Cats.java
+++ Dyr Ice/src/Cats.java
@@ -1,2 +1,13 @@
-public class Cats {
+public class Cats extends Animal {
+    public Cats(int id,String species,String origin,int lifeSpan,String temper,String allergyFriendly){
+        super(id,species,origin,lifeSpan,temper,allergyFriendly);
+    }
+    public String toString() {
+        return "id: " + this.id +
+                " species: " + this.species +
+                " origin: " + this.origin +
+                " lifeSpan: " + this.lifeSpan +
+                " temper: " + this.temper +
+                " allergyFriendly: " + this.AllergyFriendly;
+    }
 }
