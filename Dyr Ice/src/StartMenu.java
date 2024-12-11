b966b54ca2bfd8d42d0260e9d222b29b0aa80d2b
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-11 14:00:18 +0100

    StartMenu method add

--- Dyr Ice/src/StartMenu.java
+++ Dyr Ice/src/StartMenu.java
@@ -1,2 +1,14 @@
 public class StartMenu {
+    TextUI ui;
+
+    public void menu(){
+        int choice = ui.promptNumeric("Type number 1");
+
+        if(choice == 1){
+            // login()
+        }else{
+            System.out.println("Invalid. Type the number 1");
+            menu();
+        }
+    }
 }
