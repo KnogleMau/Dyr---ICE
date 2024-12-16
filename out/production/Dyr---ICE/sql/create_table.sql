1a9cccda22e87802df058ae41bad74052032a10f
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-16 13:07:09 +0100

    homeMenu search

---
+++ out/production/Dyr---ICE/sql/create_table.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS Dog;
+CREATE TABLE Dog
+(
+    id integer primary key,
+    species varchar(50) not null,
+    cost INTEGER default 0,
+    lifeSpan INTEGER not null,
+    temper text (100),
+    allergyFriendly boolean not null
+);
+
+DROP TABLE IF EXISTS Cat;
+create table Cat
+(
+   id integer primary key,
+   species varchar(50) not null ,
+   origin varchar(50) not null,
+   lifeSpan integer not null,
+   temper text not null,
+   allergyFriendly boolean not null
+);
+
+DROP TABLE IF EXISTS Job;
+CREATE TABLE Job
+(
+    id          INTEGER primary key,
+    headline    varchar(50),
+    description TEXT not null,
+    salaryHour  INTEGER default 0,
+    location    varchar(50),
+    username    varchar(25),
+    takerName   varchar(25)
+
+);
+
+
+DROP TABLE IF EXISTS LogIn;
+CREATE TABLE LogIn
+(
+    id integer default 0 primary key,
+    username varchar(30) ,
+    password varchar(30) not null
+
+
+);
+
