d95a82610c98df99cb238cbbe2f85320e1bf82c1
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-12-17 12:27:42 +0100

    new methods readJobs and printJob

--- Dyr Ice/src/Jobs.java
+++ Dyr Ice/src/Jobs.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Jobs {
     public String headLine;
     public String description;
@@ -6,18 +8,32 @@ public class Jobs {
     TextUI ui = new TextUI();
     DB db = new DB();
 
-public void createJobs(){
-    var url = "jdbc:sqlite:C:\\Users\\Mavso\\IdeaProjects\\Dyr ice ice ice 2\\Fanimals";
-    db.connect(url);
+    public ArrayList<String> readJobs() {
+        var url = "jdbc:sqlite:identifier.sqlite";
+        db.connect(url);
+        ArrayList<String> jobs = db.selectJobs();
 
-            String mHeadLine = ui.promptText("Headline");
-            String mDescription = ui.promptText("Description");
-            int mSalaryHour = Integer.parseInt(ui.promptText("Salary"));
-            String mLocation = ui.promptText("Location");
-     db.MakeJobs(mHeadLine, mDescription, mSalaryHour, mLocation);
+        return jobs;
+    }
 
+    public void createJobs() {
+        var url = "jdbc:sqlite:identifier.sqlite";
+        db.connect(url);
 
-}
+        String mHeadLine = ui.promptText("Headline");
+        String mDescription = ui.promptText("Description");
+        int mSalaryHour = Integer.parseInt(ui.promptText("Salary"));
+        String mLocation = ui.promptText("Location");
+        db.MakeJobs(mHeadLine, mDescription, mSalaryHour, mLocation);
+    }
 
-
+    public void printJobList() {
+        if (!readJobs().isEmpty()) {
+            for (String s: readJobs()) {
+                System.out.println(s);
+            }
+        } else {
+            System.out.println("No jobs shows");
+        }
+    }
 }
